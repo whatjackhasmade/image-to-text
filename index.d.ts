e49b5af07e49792edd175f88a4f958bbc8cb2850
whatjackhasmade <jack@noface.co.uk>
2021-03-13 08:29:33 +0000

    Add ObjectLiteral def

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
-declare module "*.jpg"
-declare module "*.png"
-declare module "*.svg"
-declare module "*.eot"
-declare module "*.woff2"
-declare module "*.woff"
-declare module "*.ttf"
-declare module "*.mp4"
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.eot';
+declare module '*.woff2';
+declare module '*.woff';
+declare module '*.ttf';
+declare module '*.mp4';
+
+declare type ObjectLiteral = {
+  [key: string]: any;
+};
